8ba85afe9c4974b225fef2bf2c4f93c77622b2e0
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-11-26 10:30:20 -0800

    Display username list

--- cerebro-demo.js
+++ cerebro-demo.js
@@ -3,21 +3,44 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
+    counter: function() {
       return Session.get('counter');
     }
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click button': function() {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Template.body.helpers({
+    users: function() {
+      return Meteor.users.find({}).fetch();
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
+    if (Meteor.users.find().count() === 0) {
+      Accounts.createUser({
+        username: 'username',
+        email: 'email',
+        password: 'asdfasdf',
+        profile: {
+          first_name: 'fname',
+          last_name: 'lname',
+          company: 'company',
+        }
+      });
+    }
   });
 }
